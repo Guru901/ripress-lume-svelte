4c9b428eb3a95671e096fb3183a2727e9ae68fac
Gurvinder Singh <avinashkaur842@gmail.com>
2025-10-13 17:11:24 +0530

    init

---
+++ frontend/src/App.svelte
@@ -0,0 +1,61 @@
+<script lang="ts">
+  let users = $state<any>(null);
+  let hello = $state<any>(null);
+
+  $effect(() => {
+    (async () => {
+      const response = await fetch("/hello");
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch");
+      }
+
+      const data = await response.text();
+
+      hello = data;
+    })();
+    (async () => {
+      const response = await fetch("/users");
+
+      if (!response.ok) {
+        throw new Error("Failed to fetch");
+      }
+
+      const data = await response.json();
+
+      users = data;
+    })();
+  });
+</script>
+
+<main>
+  <div style="padding: 24px">
+    <h1>Ripress + Svelte minimal demo</h1>
+    <div>
+      <h2>/hello</h2>
+      <pre>{hello ? JSON.stringify(hello, null, 2) : "Loading..."}</pre>
+    </div>
+    <div>
+      <h2>/users</h2>
+      <pre>{users ? JSON.stringify(users, null, 2) : "Loading..."}</pre>
+    </div>
+  </div>
+</main>
+
+<style>
+  .logo {
+    height: 6em;
+    padding: 1.5em;
+    will-change: filter;
+    transition: filter 300ms;
+  }
+  .logo:hover {
+    filter: drop-shadow(0 0 2em #646cffaa);
+  }
+  .logo.svelte:hover {
+    filter: drop-shadow(0 0 2em #ff3e00aa);
+  }
+  .read-the-docs {
+    color: #888;
+  }
+</style>
